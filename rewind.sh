e68868b956f80c7c6bd2db50e8650102477a4194
Дмитрий <mdv413@mail.ru>
2020-07-15 12:11:03 +0300

    Init commit

---
+++ rewind.sh
@@ -0,0 +1,21 @@
+pkill ipmi_sim &&
+
+for sensor in 'lm75' 'hwm_temp'; do
+  cp ./${sensor}_template.sdrs ./${sensor}.sdrs
+  lower_non_recoverable_temp=0
+  lower_critical_temp=0
+  lower_non_critical_temp=0
+  upper_non_critical_temp=0
+  upper_critical_temp=0
+  upper_non_recoverable_temp=0
+  source '/etc/tempcn'/${sensor}_limits
+  sed -i 's/$sensor_lnr/'${lower_non_recoverable_temp}'/g' ./${sensor}.sdrs;
+  sed -i 's/$sensor_lc/'${lower_critical_temp}'/g' ./${sensor}.sdrs;
+  sed -i 's/$sensor_lnc/'${lower_non_critical_temp}'/g' ./${sensor}.sdrs;
+  sed -i 's/$sensor_unc/'${upper_non_critical_temp}'/g' ./${sensor}.sdrs;
+  sed -i 's/$sensor_uc/'${upper_critical_temp}'/g' ./${sensor}.sdrs;
+  sed -i 's/$sensor_unr/'${upper_non_recoverable_temp}'/g' ./${sensor}.sdrs;
+done
+
+sdrcomp ./main.sdrs > /var/ipmi_sim/ipmi0/sdr.20.main;
+ipmi_sim -n &
---
+++ send_files.sh
@@ -0,0 +1,2 @@
+scp -rCq . root@192.168.101.221:/etc/ipmi
+# scp -rCq ./limits/. root@192.168.101.221:/etc/tempcn
